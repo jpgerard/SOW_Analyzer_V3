0607964a1984db4efca8a6fb3edae728ee506f4e
jpgerard <jpg02@gmail.com>
2025-02-11 19:57:45 -0500

    Update package initialization

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+"""SOW Analyzer V3 package."""
+
+# Import submodules
+from . import document_processing
+from . import analysis
+from . import extraction
+from . import reporting
+from . import utils
+
+__all__ = [
+    'document_processing',
+    'analysis',
+    'extraction',
+    'reporting',
+    'utils'
+]
